e26b11b03500102fb4f366d136a2cec77a5ee353
Amirhamidi2001 <amirarsalanh2001@gmail.com>
2023-10-08 12:54:27 +0330

    Add tags

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", blog_views, name="blog"),
     path("<int:pid>/", single_views, name="single"),
     path("category/<str:cat_name>/", blog_views, name="category"),
+    path("tag/<str:tag_name>/", blog_views, name="tag"),
     path("author/<str:author_username>/", blog_views, name="author"),
     path("search/", blog_search, name="search"),
     path("rss/feeds/", LatestEntriesFeed()),
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,8 @@ def blog_views(request, **kwargs):
     posts = Post.objects.filter(status=1)
     if kwargs.get("cat_name") != None:
         posts = posts.filter(category__name=kwargs["cat_name"])
+    if kwargs.get("tag_name") != None:
+        posts = posts.filter(tags__name__in=[kwargs['tag_name']])
     if kwargs.get("author_username") != None:
         posts = posts.filter(author__username=kwargs["author_username"])
     posts = Paginator(posts, 20)
